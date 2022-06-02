46eea19961a2dade1bf17b1848d3aed2796e38cd
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-02 15:39:06 +0300

    WIP: calculation mad value

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
-// create MAD & M estimator algorithms
-function getStandardDeviation (arr: number[]) { 
-  const n = arr.length
-  const mean = arr.reduce((a, b) => a + b) / n
-  return Math.sqrt(arr.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+function getStandardDeviation(arr: number[]) {
+  const n = arr.length;
+  const mean = arr.reduce((a, b) => a + b) / n;
+  return Math.sqrt(arr.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
 }
-const median = (arr: number[]): number => {  
+function calculateMedian(arr: number[]) {
   const s = [...arr].sort((a, b) => a - b);
   const mid = Math.floor(s.length / 2);
-  return s.length % 2 === 0 ? ((s[mid - 1] + s[mid]) / 2) : s[mid];
-};
-function mad(arr: number[]) {
-  const med = median(arr);
-  const abs = arr.map((x) => Math.abs(x - med));  
-  return median(abs);
+  return s.length % 2 === 0 ? (s[mid - 1] + s[mid]) / 2 : s[mid];
 }
-var arr = [1, 4, 3, 4, 5, 6, 7, 8, 9, 10]; // median is 5.5
-console.log(mad(arr)); // median absolute deviation is 2.5
-console.log( getStandardDeviation(arr) * 1.4826 );
+function Mestimator(arr: number[], x: number) {
+  var sumSeries = 0;
+  for (var i = arr.length - 1; i >= 0; i--) {
+    sumSeries += Number(Math.abs(arr[i] - x));
+  }
+  return (1 / (0.798 * arr.length)) * sumSeries;
+}
+function Uvalue(s: number, arr: number[]) {
+  return (1.25 * s) / Math.sqrt(arr.length);
+}
 
+var arr = [1, 2, 3, 4, 5, 6, 3, 8, 2, 20];
+var standartDeviation = getStandardDeviation(arr);
+var madex = Math.abs(standartDeviation - calculateMedian(arr)) * 1.483;
 
+var result = Uvalue(Mestimator(arr, madex), arr);
+console.log(result);
