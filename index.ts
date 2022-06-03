335b69a93bb9ca0f43537e19feefc9c378ce37a1
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-03 11:34:29 +0300

    WIP: add grubbs functions

--- index.ts
+++ index.ts
@@ -1,27 +1,6 @@
-function getStandardDeviation(arr: number[]) {
-  const n = arr.length;
-  const mean = arr.reduce((a, b) => a + b) / n;
-  return Math.sqrt(arr.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
-}
-function calculateMedian(arr: number[]) {
-  const s = [...arr].sort((a, b) => a - b);
-  const mid = Math.floor(s.length / 2);
-  return s.length % 2 === 0 ? (s[mid - 1] + s[mid]) / 2 : s[mid];
-}
-function Mestimator(arr: number[], x: number) {
-  var sumSeries = 0;
-  for (var i = arr.length - 1; i >= 0; i--) {
-    sumSeries += Number(Math.abs(arr[i] - x));
-  }
-  return (1 / (0.798 * arr.length)) * sumSeries;
-}
-function Uvalue(s: number, arr: number[]) {
-  return (1.25 * s) / Math.sqrt(arr.length);
-}
+import {calculateUvalue} from './mad'
 
 var arr = [1, 2, 3, 4, 5, 6, 3, 8, 2, 20];
-var standartDeviation = getStandardDeviation(arr);
-var madex = Math.abs(standartDeviation - calculateMedian(arr)) * 1.483;
+var Uvalue = calculateUvalue(arr);
 
-var result = Uvalue(Mestimator(arr, madex), arr);
-console.log(result);
+console.log(Uvalue);
