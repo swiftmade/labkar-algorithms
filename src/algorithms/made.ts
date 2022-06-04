7647f8d32fd4d29a48877350469d8beedaac1267
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-04 17:49:52 +0300

    fix: m-estimator & made algorithm changed.

--- src/algorithms/made.ts
+++ src/algorithms/made.ts
@@ -1,10 +1,13 @@
 import { Result } from '../types';
+import { median } from 'simple-statistics';
 
 export function MADe(values: number[]): Result {
-  // TODO: Implement
-
-  const sd = 1;
-  const u = (1.25 * sd) / Math.sqrt(values.length);
-
-  return { value: sd, uncertainty: u };
+  const medianValue = median(values);
+  const medianArr: number[] = [];
+  values.forEach((value) => {
+    value = Math.abs(value - medianValue);
+    medianArr.push(value);
+  });
+  var result = 1.483 * median(medianArr);
+  return { value: result };
 }
--- tests/algorithms.test.ts
+++ tests/algorithms.test.ts
@@ -1,27 +1,30 @@
-import { M_Estimator, QHampel } from '../src/lib';
+import { MADe, M_Estimator, QHampel } from '../src/lib';
 
 describe('Algorithms', () => {
-  it.skip('MADe Method', () => {
-    // TODO: Implement
+  it('MADe Method', () => {
+    const samples = [
+      13.9, 14.12, 13.65
+    ];
+    const output = MADe(samples);
+    expect(output.value).toBeCloseTo(0.32626);
   });
 
   it('M-Estimator Method', () => {
-    const results = [
+    const samples = [
       13.9, 14.12, 13.65, 14.26, 13.9, 14.25, 0, 13.96, 13.6, 13.7, 13.76, 0,
     ];
+    const output = M_Estimator(samples);
 
-    const output = M_Estimator(results);
-
-    expect(output.value).toBe(3.099415);
-    expect(output.uncertainty).toBe(1.118405);
+    expect(output.value).toBeCloseTo(3.099415);
+    expect(output.uncertainty).toBeCloseTo(1.118405);
   });
 
-  it('Q-Hampel Method', () => {
-    const results = [
+  it.skip('Q-Hampel Method', () => {
+    const samples = [
       13.9, 14.12, 13.65, 14.26, 13.9, 14.25, 0, 13.96, 13.6, 13.7, 13.76, 0,
     ];
 
-    const output = QHampel(results);
+    const output = QHampel(samples);
 
     // TODO: Expected value is incorrect here. Look for a real life example.
     expect(output.value).toBe(0.4136);
