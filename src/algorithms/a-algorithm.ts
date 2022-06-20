1dd7f02ac28a143c18bea4bd17342f780f6c0bcf
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-20 23:16:25 +0300

    feat: add xiValues and robustDeviation calculation

--- src/algorithms/a-algorithm.ts
+++ src/algorithms/a-algorithm.ts
@@ -3,11 +3,14 @@ import { AResult } from '../types';
 
 export function A_Algorithm(values: number[]): AResult {  
 
-  const robust = median(values); // robust value = median of the values   
+  const robust = median(values); // robust value = median of the values  
+  const xiValues = values.map(x => x - robust); // subtract the robust value from each value   
+  const robustDeviation =  median(xiValues);
+
 
   return {
     robust,
-    robustDeviation: 0,
+    robustDeviation,
     highLimit: 0,
     lowLimit: 0,
   };
