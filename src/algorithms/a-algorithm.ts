97f45f94eda9aedad4928ca22aa7b24a6d0f1cb0
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-20 22:51:57 +0300

    feat: a algorithm creeate result type

---
+++ src/algorithms/a-algorithm.ts
@@ -0,0 +1,12 @@
+import { AResult } from '../types';
+
+export function A_Algorithm(values: number[]): AResult {
+  // TODO: Implement the A-Algorithm
+
+  return {
+    robust: 0,
+    robustDeviation: 0,
+    highLimit: 0,
+    lowLimit: 0,
+  };
+}
