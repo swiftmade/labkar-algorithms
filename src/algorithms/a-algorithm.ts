a262d68545836df208f6ddfec833b07037987068
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-20 23:21:38 +0300

    feat: add limit, high limit & low limit

--- src/algorithms/a-algorithm.ts
+++ src/algorithms/a-algorithm.ts
@@ -5,13 +5,16 @@ export function A_Algorithm(values: number[]): AResult {
 
   const robust = median(values); // robust value = median of the values  
   const xiValues = values.map(x => x - robust); // subtract the robust value from each value   
-  const robustDeviation =  median(xiValues);
+  const robustDeviation =  median(xiValues) * 1.483;
+  const limit = robustDeviation * 1.5;
+  const highLimit = robust + limit; 
+  const lowLimit = robust - limit;
 
 
   return {
     robust,
     robustDeviation,
-    highLimit: 0,
-    lowLimit: 0,
+    highLimit,
+    lowLimit,
   };
 }
