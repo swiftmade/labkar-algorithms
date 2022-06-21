d79f71723b8a8b2835896270ee7f33286a018222
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-21 11:07:02 +0300

    style: add description variable

--- src/algorithms/a-algorithm.ts
+++ src/algorithms/a-algorithm.ts
@@ -1,6 +1,14 @@
 import { median } from 'simple-statistics';
 import { AResult } from '../types';
 
+/**
+ * @param {number[]} values - an array of numbers
+ * @returns An object with the following properties:
+ * - robust =  x*
+ * - robustDeviation =  s*
+ * - highLimit
+ * - lowLimit
+ */
 export function A_Algorithm(values: number[]): AResult {
   const robust = median(values); // robust value = median of the values
   const xiValues = values.map((x) => Math.abs(x - robust)); // subtract the robust value from each value
