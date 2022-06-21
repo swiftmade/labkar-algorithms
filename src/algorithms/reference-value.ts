431fc7aada1842e694a56fd32a255bd9d48b4f3d
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-21 11:55:27 +0300

    feat: function parameter change & referenceArray find

--- src/algorithms/reference-value.ts
+++ src/algorithms/reference-value.ts
@@ -1,9 +1,21 @@
 import { ReferenceResult } from '../types';
 
-export function referenceValue(x: number, formulae: Object): ReferenceResult {
-  
+export function referenceValue(
+  x: number,
+  formulae: Array<{
+    is_reference: boolean; // true if this is a reference value
+    method: string; // the method used to calculate the reference value
+    formula: string; // the formula used to calculate the reference value
+    min: number; // the minimum value of the reference value
+    max: number; // the maximum value of the reference value
+  }>
+): ReferenceResult {
+  // find is_reference = true array element
+  const referenceArray = formulae.find((formula) => formula.is_reference); // find the reference value
+  console.log(referenceArray);
+
   return {
     max: 0,
-    formula: ''    
+    formula: '',
   };
 }
