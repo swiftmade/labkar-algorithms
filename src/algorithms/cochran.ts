f7ec4577932d67fc725910a6b79398e1c8dc85fa
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-21 18:33:31 +0300

    feat: find squareDeviations

--- src/algorithms/cochran.ts
+++ src/algorithms/cochran.ts
@@ -1,5 +1,31 @@
 import { CochranResult } from '../types';
+import { standardDeviation } from 'simple-statistics';
 
-export function Cochran(values: number[]): CochranResult {
+export function Cochran(values: Array<number[]>): CochranResult {
+  values = [
+    [91, 89.6],
+    [89.7, 89.8],
+    [88, 87.5],
+    [89.2, 88.5],
+    [89, 90],
+    [88.5, 90.5],
+    [88.9, 88.2],
+    [90.1, 88.4],
+    [86, 85.8],
+    [87.6, 84.4],
+    [88.2, 87.4],
+    [91, 83.1],
+    [87.5, 87.8],
+    [87.5, 87.6],
+    [88.8, 85],
+  ];
+  const squareDeviations = values
+    .map((value) => {
+      return standardDeviation(value);
+    })
+    .map((value) => {
+      return value * value;
+    });
+
   return true;
 }
