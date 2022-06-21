f547478375652485ac2b440c81cf5d05020cff07
Burak Odabas <ahmetburakodabas@gmail.com>
2022-06-21 12:07:55 +0300

    feat: add sample data & fix referenceArray finding method

--- src/algorithms/reference-value.ts
+++ src/algorithms/reference-value.ts
@@ -10,9 +10,35 @@ export function referenceValue(
     max: number; // the maximum value of the reference value
   }>
 ): ReferenceResult {
+  formulae = [
+    {
+      formula: '0.075*X+4.6',
+      method: 'TS EN 13132',
+      min: 60.1,
+      max: 500.0,
+      is_reference: true,
+    },
+    {
+      formula: '0.016*X+3.70 ',
+      method: 'TS EN ISO 13032',
+      min: 8.0,
+      max: 50.0,
+      is_reference: true,
+    },
+    {
+      formula: '0.6*X',
+      method: 'ASTM D381',
+      min: 1.0,
+      max: 30.0,
+      is_reference: false,
+    },
+  ];
+
   // find is_reference = true array element
-  const referenceArray = formulae.find((formula) => formula.is_reference); // find the reference value
-  console.log(referenceArray);
+  const referenceArray = formulae.filter((formula) => formula.is_reference); // find the reference value
+  const notReferenceArray = formulae.filter((formula) => formula.is_reference === false); // find the not reference value
+  
+  
 
   return {
     max: 0,
