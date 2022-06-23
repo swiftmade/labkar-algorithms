9bcf9e37157559e6cb172e3c53de40539a0024a4
Burak Odabaş <ahmetburakodabas@gmail.com>
2022-06-23 17:28:53 +0300

    Reference Value Algorithm (#6)
    * feat: reference function create & result type set
    
    * feat: function parameter change & referenceArray find
    
    * feat: add sample data & fix referenceArray finding method
    
    * feat: find rangeValue [scan referenceArray and notReference Array]
    
    * feat: complete referenceValue algorithm ✅
    
    * feat: add test case1
    
    * feat: add test case2
    
    * feat: add LAST test case ✅
    
    * style: edit comment lines
    
    * refactor: improvement code style and change variable
    
    * style: remove space
    
    * fix: null type issue fixed
    
    * fix: type changed. All issues fixed ✅
    
    * style: variable name changed

--- src/types.ts
+++ src/types.ts
@@ -6,5 +6,16 @@ export type AResult = {
   robust: number;
   robustDeviation: number;
   highLimit: number;
-  lowLimit: number;  
+  lowLimit: number;
+};
+export type ReferenceResult = {
+  value: number;
+  method: string;
+};
+export type FormulaType = {
+  is_reference: boolean;
+  method: string;
+  formula: string;
+  min: number;
+  max: number;
 };
