e7f3b1af5abb9c95bfc360e19e06e1d087e684b8
Ahmet Özışık <ozisikahmet@gmail.com>
2022-12-15 12:57:43 +0200

    Cochran Test (breaking change) (#10)
    * change cochran test algorithm
    
    * bump version

--- src/algorithms/cochran.ts
+++ src/algorithms/cochran.ts
@@ -1,53 +1,57 @@
 import { get } from 'lodash';
 import { CochranResult } from '../types';
-import { standardDeviation } from 'simple-statistics';
+import { sampleStandardDeviation } from 'simple-statistics';
 import cochranCriticalValues from './cochranCriticalValues';
 
-export function Cochran(values: Array<number[]>): CochranResult | null {
+export function Cochran(
+  values: Array<number[]>,
+  options: { alpha: number } = { alpha: 0.05 }
+): CochranResult | null {
   /* p -> numune sayısı  */
   const pValue = values.length;
 
   /* n -> yapılan tekrar,test sayısı */
   const nValue = values[0].length;
 
-  const onePercentCriticalValue = get(
-    cochranCriticalValues,
-    `${nValue}.1%.${pValue}`
-  );
+  const criticalValueKey =
+    options.alpha === 0.05
+      ? `${nValue}.5%.${pValue}`
+      : `${nValue}.1%.${pValue}`;
 
-  const fivePercentCriticalValue = get(
-    cochranCriticalValues,
-    `${nValue}.5%.${pValue}`
-  );
+  const criticalValue = get(cochranCriticalValues, criticalValueKey);
 
-  if (!onePercentCriticalValue || !fivePercentCriticalValue) {
+  if (!criticalValue) {
     return null;
   }
 
   const squareDeviations = values
-    .map((value) => {
-      return standardDeviation(value);
-    })
-    .map((value) => {
-      return value * value;
-    });
+    .map((pair: number[]) => sampleStandardDeviation(pair))
+    .map((sd: number) => sd * sd);
 
   const maxDeviation = Math.max.apply(null, squareDeviations);
   const sumOfSquareDeviations = squareDeviations.reduce((a, b) => a + b, 0);
 
+  // These are the values used to calculate max deviation
+  const cPairIndex = squareDeviations.indexOf(maxDeviation);
+  const cPair = values[squareDeviations.indexOf(maxDeviation)];
+
   const cValue = maxDeviation / sumOfSquareDeviations;
 
-  if (cValue < fivePercentCriticalValue) {
-    return CochranResult.Correct;
+  if (cValue < criticalValue) {
+    return {
+      outlier: false,
+      cValue,
+      cPair,
+      cPairIndex: cPairIndex,
+      maxDeviation,
+    };
   }
 
-  if (cValue < onePercentCriticalValue && cValue > fivePercentCriticalValue) {
-    return CochranResult.Straggler;
-  }
-
-  if (cValue > onePercentCriticalValue) {
-    return CochranResult.Outlier;
-  }
-
-  return null;
+  return {
+    outlier: true,
+    cValue,
+    cPair,
+    cPairIndex,
+    maxDeviation,
+  };
 }
--- src/types.ts
+++ src/types.ts
@@ -10,11 +10,25 @@ export type AResult = {
   lowLimit: number;
 };
 
-export enum CochranResult {
-  Outlier = 'outlier',
-  Straggler = 'straggler',
-  Correct = 'correct',
-}
+export type CochranResult = {
+  outlier: boolean;
+  /**
+   * Value used to compare to the elected critical value
+   */
+  cValue: number;
+  /**
+   * The pair used to calculate the max deviation
+   */
+  cPair: number[];
+  /**
+   * Index of the pair used to calculate the max deviation
+   */
+  cPairIndex: number;
+  /**
+   * Maximum value among (stdDev(pair) ^ 2)
+   */
+  maxDeviation: number;
+};
 
 export type ReferenceResult = {
   value: number;
